7a8e5f00a40a0a1ebb16da7f8c96c6e3b4aa1eb7
mangguo04 <mangguo092504@outlook.com>
2025-10-02 23:31:45 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,9 +130,9 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "21012");
+        envVars.put("HY2_PORT", "22439");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "217012");
+        envVars.put("REALITY_PORT", "22439");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
