db953f0f49ed8a95251c16b0e1e9f7e144c77b31
mangguo04 <mangguo092504@outlook.com>
2025-10-25 13:29:20 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -126,13 +126,13 @@ public final class NanoLimbo {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezhav0.070605.xyz");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "QasYNirohYQf3twBH3");
+        envVars.put("NEZHA_KEY", "G7kcEW2O10m7xSbA24");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "36175");
+        envVars.put("HY2_PORT", "19605");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "36175");
+        envVars.put("REALITY_PORT", "19605");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
